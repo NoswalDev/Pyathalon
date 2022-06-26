7ff2b87c46721bec6eaa13ac4a3a30eef9e2b76a
NoswalDev <50437048+NoswalDev@users.noreply.github.com>
2022-06-25 20:26:33 -0700

    3Sum solved

--- Scraps/ThreeSum.py
+++ Scraps/ThreeSum.py
@@ -1,53 +1,68 @@
 output = []
 from typing import List
 '''
-[a,b,c]
-a+b+c = 0
--a = b+c
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
-[-1,0,1,2,-1,-4]
-[0,-1,-1,1,2,-4]
-[2,1,-1,-1,0,-4]
-[3,-1,-2,-2,-1]
+Notice that the solution set must not contain duplicate triplets.
 
-[-4,-1,-1,0,1,2]
-[-2,-2,-1,-1,3]
-
-loop1: if in list(sets(nums[0:-2]))
-loop2: unique
-loop3: if in nums[k:]
-
-no dupe entries
-solution sets are sorted
-are inputs sorted? yes
-
-meth2
-list to dict
-    dict = {val:ind,val:ind}
-    get unique and get correct pos when jumping
-use filter() to get subdict of specific range
-lv1[0:-2]
-lv2[lv1:-1]
-lv3[lv2:] unique
 '''
-nums = [-1,0,1,2,-1,-4]
-nums = [-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
-nums = [-2,0,1,1,2]
+arr = [-1,0,1,2,-1,-4]
+# nums = [-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
+# nums = [-2,0,1,1,2]
 
-# dict1 = {v : i for i,v in enumerate(snums)}
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    outp = []
+    done = {}
+    dupes = {}
+    complement = {}
+    t_arr = []
+    k = 0
+    mn = 0
+    mx = 0
+    for i in range(0,len(nums)-2):
+        complement = {}
+        if nums[i] in done:
+            continue
+        done[nums[i]] = -1
+        for j in range(i+1,len(nums)):
+            k = -nums[i] - nums[j]
+            if nums[j] in complement:
+                mn = min(nums[i],nums[j],k)
+                mx = max(nums[i],nums[j],k)
+                #check dupes
+                t_arr = dupes.get(mn,[])
+                if mx in t_arr:
+                    continue
+                t_arr.append(mx)
+                dupes[mn] = t_arr
+                outp.append([mn, -mn-mx, mx])
+            else:
+                complement[k] = -1
+    return outp
+'''
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    res = []
+    nums.sort()
+    
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+        
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = a + nums[l] + nums[r]
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
+            else:
+                res.append([a, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return res
+'''
 
-output = []
-snums = sorted(nums)
-dict1 = {}
-for i,v in enumerate(snums[0:-2]):
-    if not v in dict1:
-        dict1[v] = i
-for v in dict1:
-    dict2 = {w:j for j,w in enumerate(snums[1+dict1[v]:-1])}
-    for w in dict2:
-        if -v-w in snums[dict1[v]+dict2[w]+2:]:
-            output.append([v,w,-v-w])
-return output
-
+print(threeSum(arr))
 
     
